980d5cb05f5530b94a77141c512c2a25893b7ad9
Tianqing Gao <tianqing.gao@student.adelaide.edu.au>
2022-10-26 14:04:23 +1030

    something about prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import { data } from "../../../data";
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <section>
+      <h3>prop drillings</h3>
+      <List people={people} />
+    </section>
+  );
+};
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name }) => {
+  return (
+    <>
+      <div className="item">
+        <h4>SinglePerson</h4>
+        <h4>{id}</h4>
+        <h4>{name}</h4>
+      </div>
+    </>
+  );
 };
 
 export default PropDrilling;
